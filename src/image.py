7dd724d10bb76aee216bbcf8205759ccef5ffcf5
Alex Fernández <alejandrofer@gmail.com>
2009-03-10 23:27:21 +0100

    Shows an error when ImageMagick is not installed

--- src/image.py
+++ src/image.py
@@ -22,6 +22,8 @@ class Image(Container):
   start = '\\begin_inset Graphics'
   ending = '\\end_inset'
 
+  converter = True
+
   def __init__(self):
     self.parser = ImageCommand()
     self.output = ImageOutput()
@@ -29,6 +31,9 @@ class Image(Container):
 
   def process(self):
     self.url = self.header[1]
+    if not os.path.exists(self.url):
+      Trace.error('Error in image origin ' + self.url)
+      return
     self.destination = os.path.splitext(self.url)[0] + '.png'
     factor = 100
     self.convert(self.url, self.destination, factor)
@@ -36,22 +41,36 @@ class Image(Container):
 
   def convert(self, origin, destination, factor):
     "Convert an image to PNG"
-    if not os.path.exists(origin):
-      Trace.error('Error in image origin ' + origin)
+    if not Image.converter:
       return
-    if os.path.exists(destination) and os.path.getmtime(origin) <= os.path.getmtime(destination):
-      # file has not changed; do not convert
+    if origin == destination:
       return
+    if os.path.exists(destination):
+      if os.path.getmtime(origin) <= os.path.getmtime(destination):
+        # file has not changed; do not convert
+        return
     dir = os.path.dirname(destination)
     if len(dir) > 0 and not os.path.exists(dir):
       os.makedirs(dir)
-    Trace.debug('Converting ' + origin + ' to ' + destination + ' with density ' + str(factor))
-    subprocess.call('convert -density ' + str(factor) + ' ' + origin + ' ' + destination, shell=True)
+    try:
+      result = subprocess.call('convert -density ' + str(factor) + ' '
+          + origin + ' ' + destination, shell=True)
+      if result != 0:
+        Trace.error('ImageMagick not installed; images will not be processed')
+        Image.converter = False
+        return
+      Trace.debug('Converted ' + origin + ' to ' + destination
+          + ' with density ' + str(factor))
+    except OSError:
+      Trace.error('Error while converting image ' + origin)
+
 
   dimensions = dict()
 
   def getdimensions(self, filename):
     "Get the dimensions of a PNG image"
+    if not os.path.exists(filename):
+      return None, None
     if filename in Image.dimensions:
       return Image.dimensions[filename]
     pngfile = codecs.open(filename, 'rb')
