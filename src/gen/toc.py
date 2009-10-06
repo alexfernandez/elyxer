1eb6b53e9cfd418ad2fbc62b8b562090e4d7458f
chenno <chenno@vecinos-productions.org>
2009-10-06 11:13:36 +0200

    Finished moving TOC generation to gen.toc.

--- src/gen/toc.py
+++ src/gen/toc.py
@@ -19,52 +19,14 @@
 #   along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
 # --end--
-# Alex 20090919
-# eLyXer converter
+# Alex 20091006
+# eLyXer TOC generation
 # http://www.nongnu.org/elyxer/
 
 
-from io.fileline import *
-from util.options import *
 from gen.factory import *
 from gen.structure import *
-from post.postprocess import *
-from post.postlist import *
-from post.posttable import *
-from math.postformula import *
 
-
-class eLyXerConverter(object):
-  "Converter for a document in a lyx file"
-
-  def __init__(self, filein, fileout):
-    "Create the converter"
-    self.reader = LineReader(filein)
-    self.writer = LineWriter(fileout)
-
-  def convert(self):
-    "Perform the conversion for the document"
-    try:
-      if Options.toc:
-        # generate TOC
-        writer = TOCWriter(self.writer)
-        self.processcontents(lambda container: writer.writetoc(container))
-      else:
-        # generate converted document
-        self.processcontents(lambda container: self.writer.write(container.gethtml()))
-    except (Exception):
-      Trace.error('Conversion failed at ' + self.reader.currentline())
-      raise
-
-  def processcontents(self, write):
-    "Parse the contents and write it by containers"
-    factory = ContainerFactory()
-    postproc = Postprocessor()
-    while not self.reader.finished():
-      containers = factory.createsome(self.reader)
-      for container in containers:
-        container = postproc.postprocess(container)
-        write(container)
 
 class TOCWriter(object):
   "A class to write the TOC of a document."
