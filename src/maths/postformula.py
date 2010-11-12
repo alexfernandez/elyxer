aee792ee94e5fe805e3d9ee23a715c24ffba1541
chenno <elyxer@gmail.com>
2010-11-12 02:36:52 +0100

    math2html: Clean up unused postprocessing code.

--- src/maths/postformula.py
+++ src/maths/postformula.py
@@ -41,8 +41,6 @@ class PostFormula(object):
     if Options.jsmath or Options.mathjax:
       return formula
     self.postnumbering(formula)
-    #self.postcontents(formula.contents)
-    #self.posttraverse(formula)
     return formula
 
   def postnumbering(self, formula):
@@ -84,71 +82,6 @@ class PostFormula(object):
     if isinstance(function.parent, Formula) or isinstance(function.parent, FormulaRow):
       return function.parent
     return self.searchrow(function.parent)
-
-  def postcontents(self, contents):
-    "Search for sum or integral"
-    for index, bit in enumerate(contents):
-      self.checklimited(contents, index)
-      if isinstance(bit, FormulaBit):
-        self.postcontents(bit.contents)
-
-  def checklimited(self, contents, index):
-    "Check for a command with limits"
-    bit = contents[index]
-    if not isinstance(bit, EmptyCommand):
-      return
-    if not bit.command in FormulaConfig.limits['commands']:
-      return
-    limits = self.findlimits(contents, index + 1)
-    limits.reverse()
-    if len(limits) == 0:
-      return
-    tagged = TaggedBit().complete(limits, 'span class="limits"')
-    contents.insert(index + 1, tagged)
-
-  def findlimits(self, contents, index):
-    "Find the limits for the command"
-    limits = []
-    while index < len(contents):
-      if not self.checklimits(contents, index):
-        return limits
-      limits.append(contents[index])
-      del contents[index]
-    return limits
-
-  def checklimits(self, contents, index):
-    "Check for a command making the limits"
-    bit = contents[index]
-    if not isinstance(bit, SymbolFunction):
-      return False
-    if not bit.command in FormulaConfig.limits['operands']:
-      return False
-    bit.output.tag += ' class="bigsymbol"'
-    return True
-
-  def posttraverse(self, formula):
-    "Traverse over the contents to alter variables and space units."
-    last = None
-    for bit, contents in self.traverse(formula):
-      if bit.type == 'alpha':
-        self.italicize(bit, contents)
-      elif bit.type == 'font' and last and last.type == 'number':
-        bit.contents.insert(0, FormulaConstant(u' '))
-      last = bit
-
-  def traverse(self, bit):
-    "Traverse a formula and yield a flattened structure of (bit, list) pairs."
-    for element in bit.contents:
-      if element.type:
-        yield (element, bit.contents)
-      elif isinstance(element, FormulaBit):
-        for pair in self.traverse(element):
-          yield pair
-
-  def italicize(self, bit, contents):
-    "Italicize the given bit of text."
-    index = contents.index(bit)
-    contents[index] = TaggedBit().complete([bit], 'i')
 
 Postprocessor.stages.append(PostFormula)
 
